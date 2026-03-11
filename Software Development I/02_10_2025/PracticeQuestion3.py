6b48c49916dade79b8558e728224f4aaa837b883
rodkevproxy <kevinrodasuk@gmail.com>
2026-03-11 12:06:45 +0000

    Feat: Added all my folders from my laptop

---
+++ Software Development I/02_10_2025/PracticeQuestion3.py
@@ -0,0 +1,19 @@
+#Practice Question 3 
+#Do not start with cappital 
+#Adding float could be usefull in some cases 
+
+
+num_1 = float(input ("Give a number: "))
+num_2 = float(input ("Give me a second number: "))
+num_3 = float(input ("Give me a third number: "))
+
+average = (num_1 + num_2 + num_3) / 3
+
+print (f"The average is: {average}")
+
+
+
+
+#Keep it as a neew commit per day 
+
+
