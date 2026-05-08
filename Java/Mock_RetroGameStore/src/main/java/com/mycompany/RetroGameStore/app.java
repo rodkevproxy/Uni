39d7cc731514993777a5508412818116e57b02e6
rodkevproxy <kevinrodasuk@gmail.com>
2026-05-08 23:09:52 +0100

    Java App has the base code to procced with the tasks 2, 3 and 4

--- Java/Mock_RetroGameStore/src/main/java/com/mycompany/RetroGameStore/app.java
+++ Java/Mock_RetroGameStore/src/main/java/com/mycompany/RetroGameStore/app.java
@@ -1,1 +1,28 @@
-public class app {
+import java.util.Scanner;
+
+public class App {
+
+    // --- GLOBAL VARIABLES ---
+    // (Task 2: Your inventory array will go here!)
+
+    static Game[] inventory = new Game [10];
+
+
+    do 
+
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Retro Game Store!");
+        
+        // We will call your setupStore() and runMenu() methods here later!
+        
+    }
+
+    // --- YOUR METHODS WILL GO DOWN HERE ---
+    
+    // private static void runMenu() { ... }
+    // private static void setupStore() { ... }
+    // private static void buyGame() { ... }
+    // private static void saveReceipts() { ... }
+
+}
