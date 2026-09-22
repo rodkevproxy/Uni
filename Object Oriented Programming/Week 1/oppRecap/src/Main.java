470fc668485d960d74e6f745cc7724074d17a3aa
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-22 10:23:48 +0100

    fea: Updated files with the InteliJ IDE, and did activity 1, 2 and 3

--- Object Oriented Programming/Week 1/oppRecap/src/Main.java
+++ Object Oriented Programming/Week 1/oppRecap/src/Main.java
@@ -1,5 +1,40 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int quantity = 3;
+        double unitPrice = 24.99;
+        String name = "wirelessMouse";
+        boolean inStock = true;
+
+        double total = calculateTotal(quantity, unitPrice);
+        System.out.println(name + " x " + quantity + " = " + total);
+        System.out.println("In stock: " + inStock);
+
+        // Activity 3
+        String[] names = {"Mouse", "Keyboard", "GraphicCard", "Monitor"};
+        double[] prices = {24.99, 150.99, 1299.99, 599,.99};
+        double catalogueTotal = 0;
+        for (int i = 0; i < names.length; i ++){
+            catalogueTotal += prices[i];
+            if (prices[i] > 100){
+                System.out.println(names[i] + " - " + prices[i] + " - Premium");
+            }else{
+                System.out.println(prices[i] + " - " + prices[i] + " - Standard");
+            }
+            System.out.println("Catalogue: " + catalogueTotal);
+
+
+
+        }
+
+
+
     }
+    //Methods
+    //Activity 2
+    public static double calculateTotal(int quantity, double unitPrice) {
+        return quantity * unitPrice;
+        //Incompatible types
+    }
+
+
 }
