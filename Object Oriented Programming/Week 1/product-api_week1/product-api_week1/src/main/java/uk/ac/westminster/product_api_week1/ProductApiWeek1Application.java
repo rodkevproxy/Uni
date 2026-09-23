1b454e52061d0790150e2c894bac4d05c75a9041
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-23 10:34:48 +0100

    feat: Springboot

---
+++ Object Oriented Programming/Week 1/product-api_week1/product-api_week1/src/main/java/uk/ac/westminster/product_api_week1/ProductApiWeek1Application.java
@@ -0,0 +1,13 @@
+package uk.ac.westminster.product_api_week1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiWeek1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiWeek1Application.class, args);
+	}
+
+}
